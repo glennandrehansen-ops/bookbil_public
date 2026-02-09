c97260055e9d557030fec94bb57db77b1447f122
glennandrehansen-ops <glenn.andre.hansen@gmail.com>
2026-02-09 22:58:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,12 @@
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installed');
-  self.skipWaiting(); // Tving ny versjon til å aktiveres med en gang
+  console.log('Service Worker installed');
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  console.log('[Service Worker] Active');
-  return self.clients.claim(); // Ta kontroll over siden umiddelbart
+  console.log('Service Worker active');
 });
 
 self.addEventListener('fetch', (e) => {
-  // PWA krever en fetch handler, men vi lar den være tom foreløpig
-  // for å la nettleseren håndtere nettverkstrafikk som normalt.
-  // OneSignal sin importScripts ovenfor håndterer push-events.
+  // PWA krever en fetch handler for å kunne installeres
 });
